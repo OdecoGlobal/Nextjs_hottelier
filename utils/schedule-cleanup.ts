8e77e618e4fa506dc51c23d3a81296354acc5a60
OkechukwuChidera <okechukwuchidera12@gmail.com>
2025-06-27 22:39:48 +0100

    test

--- utils/schedule-cleanup.ts
+++ utils/schedule-cleanup.ts
@@ -7,7 +7,8 @@ const qstash = new Client({
 export async function scheduleDailyCleanup() {
   const response = await qstash.publish({
     url: `${process.env.NEXT_PUBLIC_API_BASE_URL}cleanup`,
-    schedule: '0 0 1 * *',
+    // schedule: '0 0 1 * *',
+    schedule: '*/2 * * * *',
     retries: 3,
   });
   return response;
